414a0dd11120e8669b555f95c44606b99ec0a8ad
Fajrian Yunus <fajrian.yunus@gmail.com>
2011-10-18 23:53:50 +0800

    adding a servlet
    Signed-off-by: Fajrian Yunus <fajrian.yunus@gmail.com>

---
+++ src/main/java/servlet/Whatever.java
@@ -0,0 +1,41 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Whatever
+ */
+@WebServlet({ "/whatever", "/wherever/*" })
+public class Whatever extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Whatever() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ServletOutputStream outputStream = response.getOutputStream();
+		outputStream.print("hello world!");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
