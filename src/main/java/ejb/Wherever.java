3e6ed8efd72ee211c41016e5909aed1918350c74
Fajrian Yunus <fajrian.yunus@gmail.com>
2011-10-19 01:01:10 +0800

    1. adding blank ejb 2. adding maven to deployment assembly

---
+++ src/main/java/ejb/Wherever.java
@@ -0,0 +1,20 @@
+package ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Wherever
+ */
+@Stateless
+@LocalBean
+public class Wherever {
+
+    /**
+     * Default constructor. 
+     */
+    public Wherever() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
